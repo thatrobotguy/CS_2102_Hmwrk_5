dfcb251778285543236996ba47d0164679e6c180
mthatfalvi <mthatfalvi@wpi.edu>
2017-04-15 16:01:53 -0400

    new updated

--- Homework_5/src/WeatherMonitor.java
+++ Homework_5/src/WeatherMonitor.java
@@ -1,5 +1,16 @@
 import java.util.GregorianCalendar;
 public class WeatherMonitor {
-
-	WeatherMonitor(){}
+	
+	//private LinkedList<
+	public WeatherMonitor(){}
+	
+	public int averageHighForMonth(int month, int year){
+		
+		return 0;
+	}
+	
+	public int averageLowForMonth(int month, int year){
+		return 0;
+	}
+	
 }
