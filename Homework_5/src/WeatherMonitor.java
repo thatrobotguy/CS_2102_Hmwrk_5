210190189bbed7b27bd98aad7855d140a51a4db3
mthatfalvi <mthatfalvi@wpi.edu>
2017-04-17 10:52:29 -0400

    t

--- Homework_5/src/WeatherMonitor.java
+++ Homework_5/src/WeatherMonitor.java
@@ -22,17 +22,10 @@ public class WeatherMonitor {
 	}
 
 	public int averageLowForMonth(int month, int year){
-<<<<<<< HEAD
-		//holdrealreading = new LinkedList<Reading>();
-		for (Reading aRead : this.readings)
-		{
-			if (aRead.inMonth(year, month))
-=======
 		int elements = 1; int totalLow = 0;
 		for (DailyWeatherReport aReport : dailyreports)
 		{			
 			if (aReport.inMonth(year, month))
->>>>>>> branch 'master' of https://github.com/thatrobotguy/CS_2102_Hmwrk_5.git
 			{
 				totalLow += aReport.getLow();				
 				elements++;	
